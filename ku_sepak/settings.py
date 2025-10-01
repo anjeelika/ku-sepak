72f1de107bc94d0fab163477e6a21eb61de57cff
anjeelika <azzahraanjelikaborselano@gmail.com>
2025-10-01 12:04:15 +0700

    Tugas 5 revisi

--- ku_sepak/settings.py
+++ ku_sepak/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "azzahra-anjelika-kusepak.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://azzahra-anjelika-kusepak.pbp.cs.ui.ac.id/"
+    "https://azzahra-anjelika-kusepak.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
